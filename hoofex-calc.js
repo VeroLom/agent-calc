2c70566771fb7905467e913c98ac64f2497321e9
VeroLom <ver@0xff.su>
2021-06-08 21:26:25 +0300

    Code refactoring

--- hoofex-calc.js
+++ hoofex-calc.js
@@ -1,40 +1,39 @@
-const HoofexCalc = {
+const AgentCalc = {
     data() {
         return {
             bathVolume: 200,
+            activeAgent: 1,
+            agentPrices: [ 245, 650, 450, 160], // copper sulfur price, Extra, new, Plus
+            agentNames: [ 'Медный купорос (сульфат меди)', 'HOOFEX EXTRA', 'HOOFEX', 'HOOFEX PLUS' ],
 
-            sulfurPrice: 245,
-            sulfurPercent1: 5,
-            sulfurPercent2: 7,
-            sulfurPercent3: 10,
-
-            hoofexPrice: 650,
-            hoofexPercent1: 1,
-            hoofexPercent2: 1.5,
-            hoofexPercent3: 2,
+            percent: {
+                1: [ 5,  1,   2, 2.5 ], // sulfur percent, Extra, new, Plus
+                2: [ 7,  1.5, 3, 3.5 ],
+                3: [ 10, 2,   4, 5   ],
+                4: [ 1,  1,   1, 7   ], // Extra percents for Hoofex Plus
+            },
         }
     },
     computed: {
-        sulfurAmount1() { return Math.round(this.sulfurPercent1 / 100 * this.bathVolume) },
-        sulfurAmount2() { return Math.round(this.sulfurPercent2 / 100 * this.bathVolume) },
-        sulfurAmount3() { return Math.round(this.sulfurPercent3 / 100 * this.bathVolume) },
-        sulfurPrice1() { return this.sulfurAmount1 * this.sulfurPrice },
-        sulfurPrice2() { return this.sulfurAmount2 * this.sulfurPrice },
-        sulfurPrice3() { return this.sulfurAmount3 * this.sulfurPrice },
-        sulfurTreatmentPrice1() { return this.sulfurPrice1 / this.bathVolume },
-        sulfurTreatmentPrice2() { return this.sulfurPrice2 / this.bathVolume },
-        sulfurTreatmentPrice3() { return this.sulfurPrice3 / this.bathVolume },
-
-        hoofexAmount1() { return Math.round(this.hoofexPercent1 / 100 * this.bathVolume) },
-        hoofexAmount2() { return Math.round(this.hoofexPercent2 / 100 * this.bathVolume) },
-        hoofexAmount3() { return Math.round(this.hoofexPercent3 / 100 * this.bathVolume) },
-        hoofexPrice1() { return this.hoofexAmount1 * this.hoofexPrice },
-        hoofexPrice2() { return this.hoofexAmount2 * this.hoofexPrice },
-        hoofexPrice3() { return this.hoofexAmount3 * this.hoofexPrice },
-        hoofexTreatmentPrice1() { return this.hoofexPrice1 / this.bathVolume },
-        hoofexTreatmentPrice2() { return this.hoofexPrice2 / this.bathVolume },
-        hoofexTreatmentPrice3() { return this.hoofexPrice3 / this.bathVolume },
+        agentPercent() {
+            return (agent, agentId) => this.percent[agentId][agent]
+        },
+        agentAmount() {
+            return (agent, agentId) => Math.round(this.percent[agentId][agent] / 100 * this.bathVolume)
+        },
+        agentPrice() {
+            return (agent, agentId) => this.agentAmount(agent, agentId) * this.agentPrices[agent]
+        },
+        agentTreatmentPrice() {
+            return (agent, agentId) => this.agentPrice(agent, agentId) / this.bathVolume
+        },
+    },
+    methods: {
+        selectAgent(agentId) {
+            console.log('New agent ' + agentId);
+            this.activeAgent = agentId;
+        }
     }
 }
 
-let app = Vue.createApp(HoofexCalc).mount('#hoofex-calc');
+let app = Vue.createApp(AgentCalc).mount('#agent-calc');
